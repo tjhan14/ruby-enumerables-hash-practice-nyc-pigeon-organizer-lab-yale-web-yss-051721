a300974ea11d194885861050186e83bdd21889a7
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-03 09:28:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+    #binding.pry
+    value.each do |inner_key, names|
+      names.each do |name|
+      binding.pry      
+      end
+    end
+    final_array
+    end
+  binding.pry
 end
+
+
