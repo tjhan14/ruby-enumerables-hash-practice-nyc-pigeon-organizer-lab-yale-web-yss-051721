248cf854d4aee1ed1c2cbec1c8b46a9708b87155
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-03 09:31:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,11 @@ def nyc_pigeon_organizer(data)
     #binding.pry
     value.each do |inner_key, names|
       names.each do |name|
-      binding.pry      
+      binding.pry    
+      if !final_array[name]
+        final_array[name] = {}
       end
+      
     end
     final_array
     end
