b45c09e545bcea1f52fed448414c3d4b96fab71f
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-03 09:39:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,8 @@ def nyc_pigeon_organizer(data)
       if !final_array[name]
         final_array[name] = {}
       end
-      if !final_array[name][key] = []
+      if !final_array[name][key]
+        final_array[name][key] = []
       end
       final_array[name][key].push(inner_key)
     end
